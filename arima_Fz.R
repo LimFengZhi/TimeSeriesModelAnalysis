fbdbfb4d0cb4a0e8704aa319bad5a5fd5ef59d7c
LIMFENGZHI <limfz-wp23@student.tarc.edu.my>
2026-04-17 08:37:49 +0000

    update arima seed 40315

--- arima_Fz.R
+++ arima_Fz.R
@@ -1,23 +1,19 @@
 library(forecast)
 
-fit <- auto.arima(train, ic = "aic", trace = TRUE)
-summary(fit)
-checkresiduals(fit)
+# Model 1: auto.arima
+fit1 <- auto.arima(train, ic = "aic", trace = TRUE)
+summary(fit1)
+checkresiduals(fit1)
+fr1 <- forecast(fit1, h = h)
+accuracy(fr1, test)
+plot(fr1, main = "auto.arima")
+lines(test, col = "turquoise2", lwd = 2)
 
-fr <- forecast(fit, h = h)
-plot(fr)
-lines(test, col = "turquoise2")
-
-accuracy(fr, test)
-
-library(forecast)
-library(tseries)
-
-fit <- arima(train, order=c(0,1,1), seasonal=list(order =c(0,1,1), period=12))
-summary(fit)
-checkresiduals(fit)
-fr <- forecast(fit, h=h)
-layout(matrix(c(1,1)))
-plot(fr)
-lines(test, col="turquoise2")
-accuracy(fr, test)
+# Model 2: SARIMA(0,1,1)(0,1,1)[12]
+fit2 <- Arima(train, order = c(0,1,1),seasonal = list(order = c(0,1,1), period = 12))
+summary(fit2)
+checkresiduals(fit2)
+fr2 <- forecast(fit2, h = h)
+accuracy(fr2, test)
+plot(fr2, main = "SARIMA(0,1,1)(0,1,1)[12]")
+lines(test, col = "turquoise2", lwd = 2)
